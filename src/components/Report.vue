9204c3fda8d443aa97f8b590b4ae670b8a78dc06
진준혁(Junhyuk Jin)/IT팀/SKP <jjhok@sk.com>
2019-02-01 22:44:24 +0900

    ing

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -19,14 +19,23 @@ import axios from "axios";
             }
             axios.post('/api/crawler', params)
             .then(response => {
-                console.log(response.data); <-- 여기까지 완료. -->
-                // this.$emit('update', response.data);
+                for (let index = 0; index < response.data[0].name.length; index++) {
+                    this.records.push({
+                        name: response.data[0].name[index],
+                        code: response.data[0].code[index].split("=")[1]
+                    }) 
+                }
             })
             .catch(response => {
                 alert("문제가 발생했습니다.");
                 console.log(response.data);
             })
 
+        },
+        data() {
+            return {
+                records: []
+            }
         },
     }
 </script>
